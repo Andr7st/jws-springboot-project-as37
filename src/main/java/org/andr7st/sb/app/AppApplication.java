336e3bd3ddf5ca2ad07a6d269653c9c371590b69
Andres Segura <dev-andr7st@outlook.com>
2021-12-09 20:47:34 -0500

    first commit

---
+++ src/main/java/org/andr7st/sb/app/AppApplication.java
@@ -0,0 +1,16 @@
+package org.andr7st.sb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
+
+
+// Starter: https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.5.7&packaging=war&jvmVersion=11&groupId=org.andr7st.sb&artifactId=app&name=app&description=Demo%20project%20for%20Spring%20Boot&packageName=org.andr7st.sb.app&dependencies=web
